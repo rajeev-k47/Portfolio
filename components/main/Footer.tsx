80b3c151de3a50293530c833e7314a277af3cfe1
rajeev-k47 <rajeevkumawat2007@outlook.com>
2025-02-05 14:07:37 +0530

    <New,Footer:*/*>

---
+++ components/main/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="w-full h-full bg-transparent text-gray-200 shadow-lg p-[15px] ">
+            <div className="mb-[20px] text-[15px] text-center">
+                &copy; 2025 Rajeev kumawat
+            </div>
+    </div>
+  )
+}
+
+export default Footer
